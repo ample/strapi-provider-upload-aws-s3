177a28450c637b4741cf85ff6a65f68fb0dc97e9
Taylor C. MacDonald <taylor@helloample.com>
2021-05-01 23:35:28 -0400

    Add support for subdirectories

--- packages/strapi-provider-upload-aws-s3/lib/index.js
+++ packages/strapi-provider-upload-aws-s3/lib/index.js
@@ -10,7 +10,7 @@ const _ = require('lodash');
 const AWS = require('aws-sdk');
 
 module.exports = {
-  init(config) {
+  init({ path: customPath, ...config }) {
     const S3 = new AWS.S3({
       apiVersion: '2006-03-01',
       ...config,
@@ -20,7 +20,9 @@ module.exports = {
       upload(file, customParams = {}) {
         return new Promise((resolve, reject) => {
           // upload file on S3 bucket
-          const path = file.path ? `${file.path}/` : '';
+          const path = customPath ? `${customPath}/`
+            : file.path ? `${file.path}/`
+            : ""
           S3.upload(
             {
               Key: `${path}${file.hash}${file.ext}`,
@@ -45,7 +47,9 @@ module.exports = {
       delete(file, customParams = {}) {
         return new Promise((resolve, reject) => {
           // delete file on S3 bucket
-          const path = file.path ? `${file.path}/` : '';
+          const path = customPath ? `${customPath}/`
+            : file.path ? `${file.path}/`
+            : ""
           S3.deleteObject(
             {
               Key: `${path}${file.hash}${file.ext}`,
